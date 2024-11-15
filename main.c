52ce55581f89fd2d631c336ec0898b8ae19eaf9b
Daniel Ortiz <daniel.ortiz@udc.es>
2024-11-15 11:01:36 +0100

    #

--- main.c
+++ main.c
@@ -13,18 +13,27 @@ void insertarMonticulo(pmonticulo m,int x);
 void quitarMenor(pmonticulo m);
 int consultarMenor(const pmonticulo m);
 void crearMonticulo(pmonticulo m,int v[],int n);
-void flotar(pmonticulo m,int x);
-void hundir(pmonticulo m,int x);
+void flotar(pmonticulo m,int i);
+void hundir(pmonticulo m,int i);
 
 int main() {
 
 }
-void iniMonticulo(pmonticulo m);
-void insertarMonticulo(pmonticulo m,int x);
+void iniMonticulo(pmonticulo m) {
+    m->ultimo = -1;
+}
+void insertarMonticulo(pmonticulo m,int x) {
+
+    if(m->ultimo == TAM-1) {
+        printf("Monticulo lleno\n");
+    }else {
+        m->ultimo++;
+        m->vector[m->ultimo] = x;
+        flotar(m,m->ultimo);
+    }
+}
 void quitarMenor(pmonticulo m);
 int consultarMenor(const pmonticulo m);
-void crearMonticulo(pmonticulo m,int v[],int n) {
-
-}
-void flotar(pmonticulo m,int x);
-void hundir(pmonticulo m,int x);
+void crearMonticulo(pmonticulo m,int v[],int n);
+void flotar(pmonticulo m,int i);
+void hundir(pmonticulo m,int i);
